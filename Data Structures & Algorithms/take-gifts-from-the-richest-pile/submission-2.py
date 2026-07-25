1a3396d5a5720321b46f05d2991f511fe987afa0
Adnan Ali <112294571+adnanAliSDE@users.noreply.github.com>
2026-07-25 09:16:25 +0530

    Add: take-gifts-from-the-richest-pile - submission-2

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts.sort()
+        n=len(gifts)
+        if n==0:
+            return None
+        for _ in range(k):
+            max_num=gifts[0]
+            mi=0
+            for idx in range(1,n):
+                if gifts[idx]>max_num:
+                    max_num=gifts[idx]
+                    mi=idx
+            gifts[mi]=floor(sqrt(max_num))
+                
+        
+        return sum(gifts)
+
+
+
+        
